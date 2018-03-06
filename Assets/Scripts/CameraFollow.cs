7a81856e2adc1f6a5c60bb4ab6b54964a561902d
韩戴 <handai@handais-Mac-mini.local>
2018-03-06 09:43:38 +0800

    初始版本

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 摄像机跟随角色移动.
+/// </summary>
+public class CameraFollow : MonoBehaviour {
+
+    private Transform m_Transform;
+
+    private Transform m_player;
+
+    public bool startFollow = false;
+
+    private Vector3 normalPos;
+
+	void Start () {
+        m_Transform = gameObject.GetComponent<Transform>();
+        normalPos = m_Transform.position;
+
+        m_player = GameObject.Find("cube_books").GetComponent<Transform>();
+
+	}
+	
+	void Update () {
+        CameraMove();
+	}
+
+    /// <summary>
+    /// 摄像机移动.
+    /// </summary>
+    void CameraMove()
+    {
+        if (startFollow)
+        {
+            //摄像机开始跟随.......
+            Vector3 nextPos = new Vector3(m_Transform.position.x, m_player.position.y + 1.5f, m_player.position.z);
+            //m_Transform.position = nextPos;
+            m_Transform.position = Vector3.Lerp(m_Transform.position, nextPos, Time.deltaTime);
+        }
+    }
+
+    public void ResetCamera()
+    {
+        m_Transform.position = normalPos;
+    }
+}
