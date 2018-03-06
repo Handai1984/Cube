81cd8d298de081174bbeb77d7512f94f97e59d3b
韩戴 <handai@handais-Mac-mini.local>
2018-03-06 09:55:59 +0800

    增加退出功能

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour {
+
+
+	public  void ExitGame()
+	{
+		//Application.Quit ();
+			 
+			
+				 UnityEditor.EditorApplication.isPlaying = false;
+			 
+				 Application.Quit();
+			 
+			
+	}
+}
